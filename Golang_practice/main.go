c724c392d2d246c3118fe0d1f6d904362ece8dda
root <root@localhost.localdomain>
2019-10-30 07:01:08 +0600

    tested golang de[

--- Golang_practice/main.go
+++ Golang_practice/main.go
@@ -2,10 +2,14 @@ package main
 import (
 	"fmt"
 	"log"
+	"github.com/rs/zerolog"
+    
 )
 
 func main(){
 	fmt.Println("Starting from here")
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+    log.Print("hello world")
 	test,rem,err := division(1,3)
     if err != nil {
 		log.Fatal(err)
